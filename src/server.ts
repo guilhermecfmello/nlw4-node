b2b24a6a6aa9ac85257dfdeadc49b2bd7dd27569
Guilherme Mello <guilhermecfmello@gmail.com>
2021-02-23 20:21:47 -0300

    Controllers, Migrations, Models and DB SQLite connections

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database';
+import { router } from './routes';
+
+
 
 const app = express();
 
-/**
- * GET => Buscar
- * POST => Salvar
- * PUT => ALterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-// http://localhost:3333/
-app.get("/", (request, response) => {
-  return response.send({ "message": "Hello World! NLW04" });
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => request,response
-app.post("/", (request, response) => {
-  // Recebeu os dados para salvar
-  return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
