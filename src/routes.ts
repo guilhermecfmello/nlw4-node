2f5756a596819c4f505c667595ae5ebbd90fb0c7
Guilherme Mello <guilhermecfmello@gmail.com>
2021-02-25 20:59:08 -0300

    Send Mail functionallity developed with nodemailer, ethereal (fake email) and handlebars (viewers)

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 import { UserController } from "./controllers/UserController";
 import { SurveysController } from "./controllers/SurveysController";
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
-const surveysController = new SurveysController;
+const surveysController = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveysController.create);
+router.post("/sendMail", sendMailController.execute);
 
 router.get("/surveys", surveysController.show);
+// router.get("/answers",));
 
 export { router };
---
+++ src/services/SendMailService.ts
@@ -0,0 +1,51 @@
+import { response } from 'express';
+import fs from 'fs';
+import handlebars from 'handlebars';
+import nodemailer, { Transporter } from 'nodemailer';
+
+
+class SendMailService {
+  private client: Transporter;
+
+
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
+      let transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass
+        }
+      });
+      this.client = transporter;
+    });
+  }
+
+  /**
+   * const resposta = await execute()
+   */
+  async execute(to: string, subject: string, variables: object, path: string) {
+
+    const templateFileContent = fs.readFileSync(path).toString("utf8");
+
+    const mailTemplateParse = handlebars.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      subject,
+      html,
+      from: "NPS <noreply@nps.com.br>"
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
+
+
+export default new SendMailService();
